2ca458c7a78af4caebfc705862cbd7b4e2e76e40
OyaYagiz <oyagiz93@gmail.com>
2024-06-23 09:06:12 +0300

    menuEdit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,17 @@
+import { useState } from "react";
 
 
 const Header = ({isDarkMode,setIsDarkMode}) => {
+    const [isOpen, setIsOpen,] = useState(false);
     
     return (
+        <section>
     <header 
     className="d-flex justify-content-between 
     align-items-center border p-4 py-2 rounded-1">
         <h1>State</h1>
         
-
+        <div className="d-flex gap-5">
         <div className="d-flex gap-3">
             <label>Koyu Mod</label>
         <input 
@@ -20,7 +23,30 @@ const Header = ({isDarkMode,setIsDarkMode}) => {
             type="checkbox" 
         />
         </div>
+
+        <div >
+            <button onClick={() => setIsOpen(!isOpen)} 
+            style={{rotate: isOpen ? '270deg' : '0deg',
+                transition: 'all 0.3s',
+             }}
+            className="btn btn-secondary">|||</button>
+        </div>
+        </div>
     </header>
+
+    {/** ALT KISIM **/}
+            {isOpen && (
+        <nav className="bg-white p-3 text-black d-flex 
+         justify-content-between rounded">
+            <span>Facebook</span>
+            <span>Instagram</span>
+            <span>Twitter</span>
+            <span>Reddit</span>
+            <span>Slack</span>
+
+        </nav>
+            )}
+    </section>
   );
 };
 
